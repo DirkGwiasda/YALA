0ce71deef7088eed2d82d5a37454e5d4fd241f93
Dirk Gwiasda <dirk@gwiasda.net>
2024-12-22 13:46:25 +0100

    git hook postcommit

---
+++ pipelines/postcommit.bat
@@ -0,0 +1,3 @@
+echo Copying appsettings.json.bak to project folder
+
+copy C:\Repos\Yala\pipelines\appsettings.json.tmp C:\Repos\Yala\src\Yala\Yala\appsettings.json
--- pipelines/postpublish.bat
+++
@@ -1,2 +0,0 @@
-echo restoring appsettings.json from pipelines folder
-copy C:\Repos\Yala\pipelines\appsettings.json.bak C:\Repos\Yala\src\Yala\Yala\appsettings.json
