f388a4833158a649f3e4621241ee45ea36819bc6
Dirk Gwiasda <dirk@gwiasda.net>
2024-12-22 13:54:45 +0100

    tst

--- pipelines/precommit.bat
+++ pipelines/precommit.bat
@@ -2,3 +2,4 @@ echo Copying appsettings.json.bak to project folder
 
 copy C:\Repos\Yala\src\Yala\Yala\appsettings.json C:\Repos\Yala\pipelines\appsettings.json.tmp
 copy C:\Repos\Yala\pipelines\appsettings.json.bak C:\Repos\Yala\src\Yala\Yala\appsettings.json
+git add "C:/Repos/Yala/src/Yala/Yala/appsettings.json"
