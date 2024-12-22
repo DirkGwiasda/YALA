8f29b305741e9e19245154748b795a2dfcb84865
Dirk Gwiasda <dirk@gwiasda.net>
2024-12-22 13:49:32 +0100

    test

--- pipelines/postcommit.bat
+++ pipelines/postcommit.bat
@@ -1,3 +1,3 @@
-echo Copying appsettings.json.bak to project folder
+echo Copying appsettings.json.tmp to project folder
 
 copy C:\Repos\Yala\pipelines\appsettings.json.tmp C:\Repos\Yala\src\Yala\Yala\appsettings.json
