d7fe473f6238efb36e364b292362d508f080c372
Dirk Gwiasda <dirk@gwiasda.net>
2024-12-22 13:43:23 +0100

    git hook precommit

---
+++ pipelines/precommit.bat
@@ -0,0 +1,4 @@
+echo Copying appsettings.json.bak to project folder
+
+copy C:\Repos\Yala\src\Yala\Yala\appsettings.json C:\Repos\Yala\pipelines\appsettings.json.tmp
+copy C:\Repos\Yala\pipelines\appsettings.json.bak C:\Repos\Yala\src\Yala\Yala\appsettings.json
